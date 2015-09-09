0e386a263ec16279f26b9cbd9822cd7b7a2a39bd
rakhmatullov <roman.rakhmatullov@livestream.com>
2015-09-09 18:00:32 +0300

    Start of decoder.

--- HuffmanDecoder/main.cpp
+++ HuffmanDecoder/main.cpp
@@ -12,30 +12,86 @@
 #include <vector>
 using namespace std;
 
-ReadBit(){
-    
-}
+class Node{
+public: 
+    char ch;
+    Node*left,*right;
+    Node(Node*l,Node*r){
+        left = l;
+        right = r;
+    }
+    Node(char cha){
+        ch=cha;
+        left=right=NULL;
+    }
+};
+bitset<8> bits;
+int count=8;
+//int currentNumber=0;
+ifstream encodedFile("../encoded.txt", ifstream::in);
+char numberOfTreeElements;
 
-ReadByte(){
+bool ReadBit(){
+    if (count==8){
+        encodedFile >> bits;
+        cout<<bits;        
+        count=0;        
+    };
+    bool bit = bits[7-count];    
+    count++;
+    return bit;
+};
+
+char ReadByte(){
+    bitset<8> chBits;
     
-}
-ReadTreeElement(){
-    
-}
+    for(int i = 0; i<8;i++){        
+        chBits[7-i]=bits[7-count];
+        count++;
+        if (count==8){
+            encodedFile >> bits;
+            cout << bits;            
+            count = 0;            
+        };
+    }
+    return static_cast<char>(chBits.to_ulong());
+};
+
+Node* ReadTreeElement(){
+  //      currentNumber++;
+        bool b = ReadBit();
+        if (b){
+            char ch = ReadByte();
+            return new Node(ch);
+        }else
+        {
+            return new Node(ReadTreeElement(), ReadTreeElement());
+        }
+};
 /*
  * 
  */
 int main(int argc, char** argv) {
-    ifstream encodedFile("../encoded.txt", ifstream::in);
+    //numberOfTreeElements = encodedFile.get();
+    //cout<< "Number of tree elements: " << (int)numberOfTreeElements <<"\n";
+    Node *topNode = ReadTreeElement();
+    Node*currentNode = topNode;
     
-    char numberOfTreeElements = encodedFile.get();
-    cout<< "Number of tree elements: " << (int)numberOfTreeElements <<"\n";
-    ReadTreeElement();
-    vector<char>symbols;
-    while(!encodedFile.eof()){
-        symbols.push_back(encodedFile.get());        
+    //vector<char>symbols;
+    while(!encodedFile.eof()&&count<8){
+        bool b = ReadBit();
+        if (!b){
+            currentNode = currentNode->left;
+        }
+        else{
+            currentNode = currentNode->right;
+        }
+        if(currentNode->left==NULL){
+           cout << currentNode->ch;
+           currentNode = topNode;
+        }
     }
-    
+    /*
     for(int i = 0; i<symbols.size();i++){
         cout << symbols[i];
     }
@@ -43,7 +99,8 @@ int main(int argc, char** argv) {
     for(vector<char>::iterator it = symbols.begin(); it!=symbols.end();it++){
         std::bitset<8> bits(*it);            
         cout << bits;
-    }
+    } 
+    */
     encodedFile.close();
     return 0;
 }
