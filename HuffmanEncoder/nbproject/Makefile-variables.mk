f6e755e8e43d4a74c5d0a1674008d4d65b90a766
rakhmatullov <roman.rakhmatullov@livestream.com>
2015-09-10 22:25:53 +0300

    encoder/decoder fixes

--- HuffmanEncoder/nbproject/Makefile-variables.mk
+++ HuffmanEncoder/nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
+CND_PLATFORM_Debug=MinGW-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/MinGW-Windows
 CND_ARTIFACT_NAME_Debug=huffmanencoder
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/huffmanencoder
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/MinGW-Windows/huffmanencoder
+CND_PACKAGE_DIR_Debug=dist/Debug/MinGW-Windows/package
 CND_PACKAGE_NAME_Debug=huffmanencoder.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/huffmanencoder.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/MinGW-Windows/package/huffmanencoder.tar
 # Release configuration
-CND_PLATFORM_Release=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
+CND_PLATFORM_Release=MinGW-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/MinGW-Windows
 CND_ARTIFACT_NAME_Release=huffmanencoder
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/huffmanencoder
-CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Release=dist/Release/MinGW-Windows/huffmanencoder
+CND_PACKAGE_DIR_Release=dist/Release/MinGW-Windows/package
 CND_PACKAGE_NAME_Release=huffmanencoder.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/huffmanencoder.tar
+CND_PACKAGE_PATH_Release=dist/Release/MinGW-Windows/package/huffmanencoder.tar
 #
 # include compiler specific variables
 #
